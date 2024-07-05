1a8bd2418acdf5b0cd218bc300604417d4a15acd
BaderAwad <145192494+BaderAwad@users.noreply.github.com>
2024-07-05 23:38:47 +0300

    Edit Screens
    add some logics to screens

--- Front-End/lib/post_management.dart
+++ Front-End/lib/post_management.dart
@@ -11,10 +11,31 @@ class PostManagement extends StatefulWidget {
 }
 
 class PostManagementState extends State<PostManagement> {
-  final postServiceController = TextEditingController();
-  Widget? lastPost;
+  final postController = TextEditingController();
+  List<String> posts = [];
+  int? editingIndex;
+
+  void _handlePost() {
+    setState(() {
+      if (editingIndex != null) {
+        posts[editingIndex!] = postController.text;
+        editingIndex = null;
+      } else {
+        posts.add(postController.text);
+      }
+      postController.clear();
+    });
+  }
+
+  void _handleEdit(int index) {
+    setState(() {
+      editingIndex = index;
+      postController.text = posts[index];
+    });
+  }
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -31,7 +52,7 @@ class PostManagementState extends State<PostManagement> {
                   ),
             ),
             const SizedBox(
-              height: 20,
+              height: 10,
             ),
             Center(
               child: Container(
@@ -56,31 +77,30 @@ class PostManagementState extends State<PostManagement> {
                               color: Theme.of(context).colorScheme.background,
                             ),
                         cursorColor: Colors.white,
-                        controller: postServiceController,
+                        controller: postController,
                         decoration: InputDecoration(
                           hintText: ('Write your post here'),
                           hintStyle: Theme.of(context)
                               .textTheme
                               .titleMedium!
                               .copyWith(
-                                color:
-                                    Theme.of(context).colorScheme.background,
+                                color: Theme.of(context).colorScheme.background,
                               ),
                           border: InputBorder.none,
                         ),
                       ),
                       const SizedBox(
-                        height: 60,
+                        height: 20,
                       ),
                       Row(
                         children: [
                           ElevatedButton(
-                            onPressed: () {},
+                            onPressed: _handlePost,
                             style: ElevatedButton.styleFrom(
                               backgroundColor: colorScheme.background,
                             ),
                             child: Text(
-                              'Post',
+                              editingIndex != null ? 'Save' : 'Post',
                               style: Theme.of(context)
                                   .textTheme
                                   .titleLarge!
@@ -111,99 +131,101 @@ class PostManagementState extends State<PostManagement> {
               ),
             ),
             const SizedBox(
-              height: 50,
+              height: 20,
             ),
             Text(
-              'Your latest post',
+              'Your latest posts',
               style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                     color: Theme.of(context).colorScheme.primary,
                   ),
             ),
             const SizedBox(
-              height: 20,
+              height: 30,
             ),
-            lastPost != null
-                ? Center(
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      width: 350,
-                      height: 200,
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.onSurface,
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(25))),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            Text(
-                              'Last Service',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleMedium!
-                                  .copyWith(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .background,
+            posts.isNotEmpty
+                ? Column(
+                    children: posts.map((post) {
+                      int index = posts.indexOf(post);
+                      return Center(
+                        child: Container(
+                          margin: const EdgeInsets.only(bottom: 10),
+                          padding: const EdgeInsets.all(16),
+                          width: 350,
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.onSurface,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(25))),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Post ${index + 1}',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .background,
+                                    ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                post,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .background,
+                                    ),
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: () => _handleEdit(index),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: colorScheme.background,
+                                    ),
+                                    child: Text(
+                                      'Edit',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleLarge!
+                                          .copyWith(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .onSurface,
+                                          ),
+                                    ),
                                   ),
-                            ),
-                            const SizedBox(
-                              height: 60,
-                            ),
-                            Row(
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: colorScheme.background,
+                                  const SizedBox(
+                                    width: 170,
                                   ),
-                                  child: Text(
-                                    'Edit',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .titleLarge!
-                                        .copyWith(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .onSurface,
-                                        ),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 170,
-                                ),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.info,
-                                    color: colorScheme.background,
-                                  ),
-                                )
-                              ],
-                            )
-                          ],
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.info,
+                                      color: colorScheme.background,
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    ),
+                      );
+                    }).toList(),
                   )
                 : Column(
                     children: [
-                      Text(
-                        'Last Service',
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium!
-                            .copyWith(
-                              color: Theme.of(context).colorScheme.background,
-                            ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
                       Text(
                         'No posts yet',
                         style: Theme.of(context).textTheme.bodyLarge!.copyWith(
@@ -219,7 +241,7 @@ class PostManagementState extends State<PostManagement> {
                         size: 75,
                       ),
                     ],
-                  )
+                  ),
           ],
         ),
       ),
--- Front-End/lib/tabs.dart
+++ Front-End/lib/tabs.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:freelancing/DashboardScreens/freelancer_dashboard.dart';
 import 'package:freelancing/post_management.dart';
 import 'package:freelancing/service_management.dart';
+import 'package:freelancing/team_management.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  State<TabsScreen> createState() {
-    return TabsScreenState();
-  }
+  State<TabsScreen> createState() => TabsScreenState();
 }
 
 class TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
+
   void _selectPage(int index) {
     setState(() {
       _selectedPageIndex = index;
@@ -21,30 +21,35 @@ class TabsScreenState extends State<TabsScreen> {
   }
 
   @override
-  Widget build(context) {
-    Widget activePage = const FreelancerDashboard();
-    var activePageTitle = 'Dashboard';
-    if (_selectedPageIndex == 1) {
-      activePage = const ServiceManagement();
-      activePageTitle = 'Service Management';
-    }
-    if (_selectedPageIndex == 2) {
-      activePage = const PostManagement();
-      activePageTitle = 'Post Management';
-    }
-    var leadingIcons = IconButton(
-        onPressed: () {},
-        icon: Icon(
-          Icons.arrow_back_ios_new,
-          color: Theme.of(context).colorScheme.primary,
-        ));
+  Widget build(BuildContext context) {
+    List<Widget> pages = [
+      const FreelancerDashboard(),
+      const ServiceManagement(),
+      const PostManagement(),
+      const TeamManagement(),
+    ];
+
+    List<String> pageTitles = [
+      'Dashboard',
+      'Service Management',
+      'Post Management',
+      'Team Management',
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        leading: leadingIcons,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
         title: Text(
-          activePageTitle,
+          pageTitles[_selectedPageIndex],
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
                 color: Theme.of(context).colorScheme.primary,
+                fontSize: 27
               ),
         ),
         centerTitle: true,
@@ -52,15 +57,19 @@ class TabsScreenState extends State<TabsScreen> {
           IconButton(
             onPressed: () {},
             icon: const CircleAvatar(
-              backgroundImage: AssetImage('assets/images/Avatar.jpg'),
+              backgroundImage: AssetImage('assets/images/Avatar.png'),
             ),
-          )
+          ),
         ],
       ),
-      body: activePage,
+      body: pages[_selectedPageIndex], 
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         currentIndex: _selectedPageIndex,
+        backgroundColor: Theme.of(context).backgroundColor,
+        selectedItemColor: Theme.of(context).colorScheme.onSurface,
+        unselectedItemColor:
+            Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.dashboard),
@@ -73,6 +82,10 @@ class TabsScreenState extends State<TabsScreen> {
           BottomNavigationBarItem(
             icon: Icon(Icons.post_add),
             label: 'Post',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.groups_2_rounded),
+            label: 'Teams',
           ),
         ],
       ),
