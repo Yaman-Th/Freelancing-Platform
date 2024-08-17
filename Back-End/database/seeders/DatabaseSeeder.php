041d6ac4dc24772491d04abc7a5fa8e6932979df
BesherElMajzoub <88548828+BesherElMajzoub@users.noreply.github.com>
2024-08-17 09:42:26 +0300

    edit seeder

--- Back-End/database/seeders/DatabaseSeeder.php
+++ Back-End/database/seeders/DatabaseSeeder.php
@@ -24,5 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(AdminSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(PostSeeder::class);
+        $this->call(ServiceSeeder::class);
     }
 }
